1542847c1e8106ee3cf7e71a50c6e872af4671f4
Stanislav Stoyanov <grimerssy@gmail.com>
2024-06-04 15:59:39 +0200

    view single book

--- Task2/apz-pzpi-21-1-stoianov-stanislav-task2/src/books/view.rs
+++ Task2/apz-pzpi-21-1-stoianov-stanislav-task2/src/books/view.rs
@@ -1,4 +1,6 @@
-use crate::{database::Database, libraries::LibraryId, state::AppState};
+use crate::{
+    database::Database, libraries::LibraryId, state::AppState, telemetry, Error,
+};
 
 use super::{
     author::Author, genre::Genre, name::Name, year::Year, Book, BookId,
@@ -26,6 +28,28 @@ pub async fn list_library_books(
         })
 }
 
+#[tracing::instrument(skip(state))]
+pub async fn view_book(
+    library_id: LibraryId,
+    book_id: BookId,
+    state: AppState,
+) -> crate::Result<Book> {
+    let library_id = library_id.sql_id(&state.id_cipher)?;
+    let book_id = book_id.sql_id(&state.id_cipher)?;
+    get_book(library_id, book_id, &state.database)
+        .await
+        .and_then(|book| {
+            book.ok_or(Error::NotFound).inspect_err(telemetry::debug)
+        })
+        .map(|book| Book {
+            id: BookId::new(book.id, &state.id_cipher),
+            year: book.year,
+            name: book.name,
+            genre: book.genre,
+            author: book.author,
+        })
+}
+
 #[derive(Clone, Debug, sqlx::FromRow)]
 struct DbBook {
     id: i64,
@@ -50,5 +74,26 @@ async fn get_library_books(
     .bind(library_id)
     .fetch_all(db)
     .await
-    .map_err(crate::Error::from)
+    .map_err(Error::from)
+}
+
+#[tracing::instrument(skip(db), err(Debug))]
+async fn get_book(
+    library_id: i64,
+    book_id: i64,
+    db: &Database,
+) -> crate::Result<Option<DbBook>> {
+    sqlx::query_as(
+        "
+        select id, year, name, genre, author
+        from books
+        where id = $1
+          and library_id = $2;
+        ",
+    )
+    .bind(book_id)
+    .bind(library_id)
+    .fetch_optional(db)
+    .await
+    .map_err(Error::from)
 }
